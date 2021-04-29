495f83dd57c10be827ac72d7474928b5caa872fe
John <jlawrencesha@gmail.com>
2021-04-29 14:43:46 -0700

    beginnings of kata two required code.

---
+++ app/src/main/java/com/giftofhappiness/android/recyclerviewkatas/models/BlogPost.kt
@@ -0,0 +1,19 @@
+package com.giftofhappiness.android.recyclerviewkatas.models
+
+data class BlogPost(
+
+    var title : String,
+    var body: String,
+    var image: String,
+    var username: String
+
+
+
+
+) {
+
+
+
+
+
+}
